de9f449cdde6b976157909350162a18564c597f8
LakoR15 <alexvolkov1994@gmail.com>
2015-07-22 15:51:04 +0300

    Исправленна ошибка null на входе

--- src/main/java/ru/edabudet/controller/api/Rooms.java
+++ src/main/java/ru/edabudet/controller/api/Rooms.java
@@ -20,24 +20,26 @@ public class Rooms extends BaseController {
 
         post("/rooms/create/:password", (request, response) -> {
 
-            Long id = roomLogic.createRoom(request.params("password"));
-            return id;
-
+                Long id = roomLogic.createRoom(request.params("password"));
+                return id;
         });
 
         post("/rooms/connect", (request, response) -> {
 
-            Room room = new Gson().fromJson(request.body(), Room.class);
+            try{
+                Room room = new Gson().fromJson(request.body(), Room.class);
+                if (roomLogic.connectRoom(room.getId(), room.getPassword())){
+                    Gson gson = new GsonBuilder()
+                            .setPrettyPrinting()
+                            .create();
+                    String json = gson.toJson(productListLogic.getProductList(room.getId()));
+                    return json;
 
-            if (roomLogic.connectRoom(room.getId(), room.getPassword())){
-                Gson gson = new GsonBuilder()
-                        .setPrettyPrinting()
-                        .create();
-                String json = gson.toJson(productListLogic.getProductList(room.getId()));
-                return json;
+                }else return "Not logged in";
 
-            }else return "Not logged in";
-
+            }catch (Exception e){
+                throw new RuntimeException("Ошибка в JSON " + e.getMessage());
+            }
         });
     }
 }
